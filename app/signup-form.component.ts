5df476bfe815a55db560b4142a6e3ef363ad1ed9
greg2 <greg@gregdev.milkdog.net>
2017-03-06 03:53:39 +0000

    control groups up the waz

---
+++ app/signup-form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from 'angular2/core';
+import { ControlGroup, Control, Validators, FormBuilder } from 'angular2/common';
+
+@Component({
+    selector: 'signup-form',
+    templateUrl: 'app/signup-form.component.html',
+    styles:
+        [`
+        .gregStyle {
+            border: 1px solid red;
+        }
+        `],
+        directives: ['ControlGroup', 'Control']
+})
+
+export class SignupFormComponent {
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+//    form = new ControlGroup({
+//        username: new Control('', Validators.required),
+//        password: new Control('', Validators.required)
+//    });
+
+    signup(){
+        console.log(this.form.value);
+    }
+}
+
